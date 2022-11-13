8b0aa69fec71426d569ff64cc57377e44ba5d337
Marshroom <88717385+MilkMouseBearCoder@users.noreply.github.com>
2022-11-13 14:50:16 +1100

    Update rayfunctions.h

--- rayfunctions.h
+++ rayfunctions.h
@@ -2,18 +2,8 @@
 #define RAYFUNCTIONS_H
 
 #include <raylib.h>
+#include <raymath.h>
 #include <math.h>
-
-// Normalize normalizes a given Vector2
-
-Vector2 RfuncNormalize(Vector2 v)
-{
-    float length = sqrt((v.x * v.x) + (v.y * v.y));
-    if (length != 0)
-        return Vector2 {v.x / length, v.y / length};
-    
-    return v;
-}
 
 // Look at rotates an object to point/look at a given point. It returns an angle in degrees
 
@@ -28,7 +18,7 @@ float RfuncLookAt(Vector2 pos, Vector2 target)
 
 Vector2 RfuncMoveTowards(Vector2 pos, Vector2 target, float speed)
 {
-    Vector2 new_pos = pos + normalize(target - pos) * speed;
+    Vector2 new_pos = pos + Normalize(target - pos) * speed;
 
     return new_pos;
 }
